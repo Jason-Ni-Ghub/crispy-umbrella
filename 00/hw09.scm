96e69797a932924499a077f76e8e1030ae944a1a
Jason Ni <jasonni6742@gmail.com>
2026-09-21 20:21:15 -0400

    Add hw09.scm with functions for calculations

---
+++ 00/hw09.scm
@@ -0,0 +1,15 @@
+> ; JASON NI
+  ; Silent Giggles
+  ; Foundations in CS
+  ; HW09 -- Automate
+  ; 2026-09-21
+  ; time spent: 0.5hr
+> (define foo (lambda (d) (* d 1)))
+> (foo 6) ; should be 6
+6
+> (define areaCirc (lambda (r) (* pi (expt r 2))))
+> (areaCirc 2) ; should be around 12.57
+12.566370614359172
+> (define areaWasher (lambda (x y) (- (* pi (expt x 2)) (* pi (expt y 2)))))
+> (areaWasher 2 1) ; should be around 9.42
+9.42477796076938
